f5d9c79b47a983af6ecfbdd367ecca3d338b00db
Hariharan <33338812+hariharancse11@users.noreply.github.com>
2023-06-02 21:49:06 +0530

    Update settings.py

--- test/uptest/uptest/settings.py
+++ test/uptest/uptest/settings.py
@@ -77,11 +77,14 @@ WSGI_APPLICATION = 'uptest.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': 'mydb_sohk',
+            'USER': 'root',
+            'PASSWORD': 'EebDhmhFha72SzyRHVUexRGnzG0PPWXk',
+            'HOST': 5432
+        }
     }
-}
 
 
 # Password validation
